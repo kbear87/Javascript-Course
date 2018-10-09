2c9f11fd2e4b7788c8d1223e745bdaf327fcf18e
Claire Carroll <claire@kbeartech.co.uk>
2018-10-09 11:40:21 +0100

    stuff

---
+++ working/5-advanced-JS/starter/function-constructors.js
@@ -0,0 +1,24 @@
+var Cat = function(name, breed, color, yearOfBirth) {  // Create main object.
+  this.name = name;
+  this.breed = breed;
+  this.color = color;
+  this.yearOfBirth = yearOfBirth;
+}
+
+Cat.prototype.numberOfPaws = function() {
+  console.log(this.name + ' has 4 paws');
+}
+
+Cat.prototype.calculateAge = function() {
+  console.log(2018 - this.yearOfBirth);
+}
+
+var sweetness = new Cat('Sweetness', 'Forest', 'Black', 2011);    // These are instances of the cat object.
+var chuckles = new Cat('Chuckles', 'Maine Coon', 'Brown', 2013);
+var claws = new Cat('Claws', 'Chartreux', 'Grey', 2007);
+
+claws.calculateAge();
+chuckles.calculateAge();
+sweetness.calculateAge();
+
+claws.numberOfPaws();
