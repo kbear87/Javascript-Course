435f3e6eff13ba516409cdeba6b56939251b3619
Claire Carroll <claire@kbeartech.co.uk>
2018-10-09 11:41:02 +0100

    saved changes

--- working/5-advanced-JS/starter/objects.js
+++ working/5-advanced-JS/starter/objects.js
@@ -1,1 +1,15 @@
 // Object.create method
+
+
+//First create an object that will act as the prototype.
+var personProto = {
+  calculateAge() {
+    console.log(2018 - this.yearOfBirth);
+  }
+};
+
+var john = Object.create(personProto, {
+  name: { value: 'John'},
+  yearOfBirth: { value: 1990},
+  job: {value: 'teacher'}
+});
