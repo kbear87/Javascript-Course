11881f48d322ad618733a635c34300008f0afd52
dean <dean@kbeartech.co.uk>
2018-10-09 12:38:46 +0000

    more learnings

--- working/5-advanced-JS/starter/objects.js
+++ working/5-advanced-JS/starter/objects.js
@@ -13,3 +13,20 @@ var john = Object.create(personProto, {
   yearOfBirth: { value: 1990},
   job: {value: 'teacher'}
 });
+
+
+
+//Primitives vs Objects
+
+//Objects
+var kitty1 = {
+  name: 'Fluffy',
+  age: 4
+}
+
+var kitty2 = kitty1;    // variable kitty2 points to the kitty1 object.  It is not an object in itself.
+kitty1.age = 5;
+console.log(kitty2.age);
+console.log(kitty1.age);
+
+//functions
