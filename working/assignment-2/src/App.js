1084905f7d8c2a3f9fad8aa125eb95854947eea4
dean <dean@kbeartech.co.uk>
2018-11-06 11:51:01 +0000

    assignment-2

--- working/assignment-2/src/App.js
+++ working/assignment-2/src/App.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
-
-
+import Validation from './Validation/Validation';
+import Character from './Character/Character';
 
 class App extends Component {
+  state = {
+    userinput: ''
+  }
 
+  changeUserInputHandler = (event) => {
+    this.setState({
+      userinput: event.target.value
+    })
+  }
 
   render() {
+    const characterList = this.state.userinput.split('').map(character => {
+      return <Character
+               character = {character}>
+             </Character>
+    })
     return (
       <div>
-
+         <input type="text" onChange={this.changeUserInputHandler} value={this.state.userinput}/>
+         <p>{this.state.userinput}</p>
+         <Validation
+           textLength={this.state.userinput.length}>
+         </Validation>
+         {characterList}
       </div>
     );
   }
